06f38bdf7210417da7858e417dbf9a0dbef3c886
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2024-03-25 16:44:50 -0500

    Home update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,39 @@
-import { Link } from "react-router-dom";
-import Background from "../components/common/Background";
+// Import React and necessary components from React Router and MDBReact
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
 
+// If Background component has props, define them in an interface
+// import Background from "../components/common/Background"; // Normal JS import
+import Background from '../components/common/Background'; // TypeScript assumes .tsx if you're in a TS project
+
+// Home component as a Functional Component in TypeScript
 const Home: React.FC = () => {
-  return (
-    <>
-    <Background>
-      <h1>Hello, gonorreas </h1>
-      <Link type="button" className="btn btn-link" to="/signup">
-        Don't have an account? Sign Up
-      </Link>
-      <Link type="button" className="btn btn-link" to="/login">
-        Already have an account? Log In
-      </Link>
-    </Background>
-    </>
-  );
+    return (
+        <Background> {/* Assuming Background is properly typed for TypeScript */}
+            <MDBContainer className="my-5 gradient-form">
+                <div className="d-flex flex-column align-items-center">
+                    <div className="text-center">
+                        <img
+                            src="../../public/maticas-tech-logo.svg"
+                            style={{ width: '185px' }}
+                            alt="Maticas Tech Logo"
+                        />
+                        <h4 className="mt-1 mb-5 pb-1">Welcome! Gonorreas</h4>
+                    </div>
+
+                    <div className="text-center pt-1 mb-5 pb-1">
+                        <Link to="/login">
+                            <MDBBtn className="mb-4 w-100 gradient-custom-2">Already have an account? Log In</MDBBtn>
+                        </Link>
+                        <Link to="/signup">
+                            <MDBBtn className="mb-4 w-100 gradient-custom-2">Don't have an account? Sign Up</MDBBtn>
+                        </Link>
+                    </div>
+                </div>
+            </MDBContainer>
+        </Background>
+    );
 };
 
 export default Home;
